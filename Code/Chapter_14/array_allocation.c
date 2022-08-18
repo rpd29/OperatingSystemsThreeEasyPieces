7b521bb16b22527ef311d50a1f6a1178ceebef0a
rpd29 <111395743+rpd29@users.noreply.github.com>
2022-08-18 10:02:48 -0700

    Delete Code directory

--- Code/Chapter_14/array_allocation.c
+++
@@ -1,9 +0,0 @@
-//allocate memory for an array of 100 integers and set array[100] equal to a value to see what happens when using debugging tools gdb and valgrind
-
-#include <stdlib.h>
-#include <stdio.h>
-
-int main(int argc, char *argv[]) {
-	 int *data = (int *) malloc(100*sizeof(int));
-	 data[100] = 0; 
-}
